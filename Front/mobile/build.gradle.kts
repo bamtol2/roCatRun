f3324b670e789bae353f5317368b3970dfcacf4b
garam0107 <name3937@gmail.com>
2025-02-03 10:31:13 +0900

    [#S12P11E205-95][FE] Chore: 구글 로그인 빌드 수정

--- Front/mobile/build.gradle.kts
+++ Front/mobile/build.gradle.kts
@@ -48,6 +48,9 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(platform(libs.firebase.bom))
     implementation(libs.play.services.auth)
+    implementation("androidx.credentials:credentials:1.3.0")
+    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
 
     // Retrofit
     implementation(libs.retrofit)
