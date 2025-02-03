0bc9d4a57a87804e8c064c2812d2647d5a6ac27f
이혜령 <dlgofud123@naver.com>
2025-02-03 15:16:46 +0900

    [#S12P11E205-101][FE] Design: 게임 실행 페이지 구현

--- Front/mobile/src/main/java/com/eeos/rocatrun/game/GameroomScreen.kt
+++ Front/mobile/src/main/java/com/eeos/rocatrun/game/GameroomScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -336,7 +335,6 @@ fun CodeGenerationSection(
 ) {
     val clipboardManager = LocalClipboardManager.current
     val context = LocalContext.current
-    val scope = rememberCoroutineScope()
 
     Row(
         modifier = Modifier
