49ee36799c2fbda5f3895b43165adf84de34b439
seungwoo <fatmunk7504@gmail.com>
2025-02-06 19:08:07 +0900

    [#S12P11E205-119][BE] Feat: 캐릭터 생성 구현 완료

--- Back/src/main/java/com/ssafy/roCatRun/api/controller/auth/JwtTokenController.java
+++ Back/src/main/java/com/ssafy/roCatRun/api/controller/auth/JwtTokenController.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-
+// hi
 @Slf4j
 @RestController
 @RequiredArgsConstructor
