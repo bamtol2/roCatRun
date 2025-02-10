8e4c7e4f043c0b4a29dea3b72b83201ab1bea919
PoloCeleste <voidness12@gmail.com>
2025-02-10 11:31:25 +0900

    [#S12P11E205-149][FE] Feat: GPX 파일 받아오기 추가

--- Front/mobile/src/main/java/com/eeos/rocatrun/game/GameplayActivity.kt
+++ Front/mobile/src/main/java/com/eeos/rocatrun/game/GameplayActivity.kt
@@ -1,5 +1,7 @@
 package com.eeos.rocatrun.game
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -15,6 +17,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.core.content.FileProvider
 import com.eeos.rocatrun.socket.SocketHandler
 import com.eeos.rocatrun.ui.theme.RoCatRunTheme
 import com.google.android.gms.wearable.DataClient
@@ -24,14 +27,22 @@ import com.google.android.gms.wearable.DataItem
 import com.google.android.gms.wearable.DataMapItem
 import com.google.android.gms.wearable.Wearable
 import com.google.android.gms.wearable.PutDataMapRequest
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import org.json.JSONObject
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 class GamePlay : ComponentActivity(), DataClient.OnDataChangedListener {
     private lateinit var dataClient: DataClient
     private var runningData by mutableStateOf<RunningData?>(null)
     private var resultData by mutableStateOf<ResultData?>(null)
     private var playersData by mutableStateOf<PlayersData?>(null)
-    
+    private var gpxFileReceived by mutableStateOf(false)
+
     // 실시간 러닝 데이터
     data class RunningData(
         val totalDistance: Double,
@@ -78,7 +89,10 @@ class GamePlay : ComponentActivity(), DataClient.OnDataChangedListener {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GameplayScreen()
+                    GameplayScreen(
+                        gpxFileReceived = gpxFileReceived,
+                        onShareClick = { shareLatestGpxFile() }
+                    )
                 }
             }
         }
@@ -102,7 +116,7 @@ class GamePlay : ComponentActivity(), DataClient.OnDataChangedListener {
                     "/running_data" -> processRunningData(dataItem)
                     "/final_result_data" -> processResultData(dataItem)
                     "/use_item" -> processUseItem(dataItem)
-//                    "/gpx_data" -> processGpxData(dataItem)
+                    "/gpx_data" -> processGpxData(dataItem)
                 }
             }
         }
@@ -156,6 +170,65 @@ class GamePlay : ComponentActivity(), DataClient.OnDataChangedListener {
         // 웹소켓 이벤트 송신 - useItem
         SocketHandler.mSocket.emit("useItem")
     }
+
+
+
+    private fun processGpxData(dataItem: DataItem) {
+        val asset = DataMapItem.fromDataItem(dataItem).dataMap.getAsset("gpx_file")
+        asset?.let {
+            GlobalScope.launch(Dispatchers.IO) {
+                try {
+                    val response = Wearable.getDataClient(this@GamePlay).getFdForAsset(it).await()
+                    response.inputStream?.use { stream ->
+                        val gpxContent = stream.bufferedReader().use { it.readText() }
+                        saveGpxFile(gpxContent)
+                        gpxFileReceived = true
+                    }
+
+                } catch (e: IOException) {
+                    Log.e("GameMulti", "Error processing GPX data", e)
+                }
+            }
+        }
+    }
+
+    private fun saveGpxFile(content: String) {
+        try {
+            val file = File(getExternalFilesDir(null), "activity_${System.currentTimeMillis()}.gpx")
+            FileOutputStream(file).use { it.write(content.toByteArray()) }
+            Log.d("GameMulti", "GPX file saved: ${file.absolutePath}")
+        } catch (e: IOException) {
+            Log.e("GameMulti", "Error saving GPX file", e)
+        }
+    }
+
+    private fun shareLatestGpxFile() {
+        val directory = getExternalFilesDir(null)
+        val gpxFiles = directory?.listFiles { file -> file.name.endsWith(".gpx") }
+
+        gpxFiles?.maxByOrNull { it.lastModified() }?.let { latestFile ->
+            shareGpxFile(this, latestFile.name)
+        } ?: run {
+            // GPX 파일이 없을 경우 처리
+            Log.e("GameMulti", "No GPX files found to share")
+        }
+    }
+
+    private fun shareGpxFile(context: Context, fileName: String) {
+        val file = File(context.getExternalFilesDir(null), fileName)
+        val uri = FileProvider.getUriForFile(context, "${context.packageName}.fileprovider", file)
+
+        val shareIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_STREAM, uri)
+            type = "application/gpx+xml"
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+
+        context.startActivity(Intent.createChooser(shareIntent, "GPX 파일 공유"))
+    }
+
+
 
     // 웹소켓 - 실시간 러닝데이터 송신
     private fun updateRunDataSocket(
