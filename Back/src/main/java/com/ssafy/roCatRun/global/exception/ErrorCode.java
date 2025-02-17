de505dcafab13f7278f81fa4ab4247a58b205191
bamtol2 <fatmunk7504@gmail.com>
2025-02-17 15:46:17 +0900

    [#S12P11E205-170][BE] Minor: 닉네임관련 제약사항 변경

--- Back/src/main/java/com/ssafy/roCatRun/global/exception/ErrorCode.java
+++ Back/src/main/java/com/ssafy/roCatRun/global/exception/ErrorCode.java
@@ -12,7 +12,7 @@ public enum ErrorCode {
 
     // 닉네임 관련 에러 코드 추가
     NICKNAME_EMPTY("닉네임은 비어있을 수 없습니다"),
-    NICKNAME_LENGTH_INVALID("닉네임은 2자 이상 10자 이하여야 합니다"),
+    NICKNAME_LENGTH_INVALID("닉네임은 2자 이상 6자 이하여야 합니다"),
     NICKNAME_PATTERN_INVALID("닉네임은 한글, 영문, 숫자만 사용 가능합니다"),
     NICKNAME_DUPLICATE("이미 존재하는 닉네임입니다");
 
