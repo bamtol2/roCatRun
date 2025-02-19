e29358d1d391b350d03724363d35c3712bc54c0a
이혜령 <dlgofud123@naver.com>
2025-02-19 23:09:45 +0900

    [#S12P11E205-176][FE] Minor: 워치에 게임 결과 송신 수정

--- Front/mobile/src/main/java/com/eeos/rocatrun/service/GamePlayService.kt
+++ Front/mobile/src/main/java/com/eeos/rocatrun/service/GamePlayService.kt
@@ -291,22 +291,6 @@ class GamePlayService : Service(), DataClient.OnDataChangedListener {
                 val cleared = responseJson.optBoolean("cleared", false)
                 Log.d("Socket", "gameResult 이벤트 수신: $responseJson")
 
-                // 워치에 게임 종료 & 결과 메세지 보내기
-                val putDataMapRequest = PutDataMapRequest.create("/game_end")
-                putDataMapRequest.dataMap.apply {
-                    putBoolean("gameEnd", true)
-                    putBoolean("cleared", true)
-                    putLong("timestamp", System.currentTimeMillis())
-                }
-                val request = putDataMapRequest.asPutDataRequest().setUrgent()
-                dataClient.putDataItem(request)
-                    .addOnSuccessListener { _ ->
-                        Log.d("Wear", "게임 종료")
-                    }
-                    .addOnFailureListener { exception ->
-                        Log.e("Wear", "게임 종료 실패", exception)
-                    }
-
                 // myResult 파싱
                 responseJson.optJSONObject("myResult")?.let { myResultJson ->
                     val result = GamePlay.MyResultData(
@@ -385,6 +369,8 @@ class GamePlayService : Service(), DataClient.OnDataChangedListener {
                         }
                     }
                 }
+
+                sendGameResult(cleared)
             }
         }
 
@@ -499,6 +485,24 @@ class GamePlayService : Service(), DataClient.OnDataChangedListener {
                 }
             }
         }
+    }
+
+    private fun sendGameResult(cleared: Boolean) {
+        // 워치에 게임 종료 & 결과 메세지 보내기
+        val putDataMapRequest = PutDataMapRequest.create("/game_end")
+        putDataMapRequest.dataMap.apply {
+            putBoolean("gameEnd", true)
+            putBoolean("cleared", true)
+            putLong("timestamp", System.currentTimeMillis())
+        }
+        val request = putDataMapRequest.asPutDataRequest().setUrgent()
+        dataClient.putDataItem(request)
+            .addOnSuccessListener { _ ->
+                Log.d("Wear", "게임 종료")
+            }
+            .addOnFailureListener { exception ->
+                Log.e("Wear", "게임 종료 실패", exception)
+            }
     }
 
     // 워치에서 실시간 러닝데이터 받아오는 함수
