269a368ba76a1592bfb9240e35a565848b33158b
PoloCeleste <voidness12@gmail.com>
2025-02-20 12:42:08 +0900

    [#S12P11E205-181][FE] 0.0.14 빌드 버전 올림

--- Front/wear/build.gradle.kts
+++ Front/wear/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.eeos.rocatrun"
         minSdk = 31
         targetSdk = 34
-        versionCode = 340130101
-        versionName = "0.0.13"
+        versionCode = 340140101
+        versionName = "0.0.14"
 
     }
 
